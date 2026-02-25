8987c7ae100f6b1e8a70459fd90ddf541679d430
MagdielPaulo <81927049+MagdielPaulo@users.noreply.github.com>
2026-02-25 06:06:43 -0300

    Fix indentation in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ''); // Use process.cwd() para garantir o caminho
+  const env = loadEnv(mode, process.cwd(), ''); 
   
   return {
     plugins: [tailwindcss()],
